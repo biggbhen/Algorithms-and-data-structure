2a0afc93428d4a14a7f6f377fd857081c2eaa424
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-08-09 19:14:39 +0100

    done

--- Wherefore art thou/app.js
+++ Wherefore art thou/app.js
@@ -4,20 +4,12 @@
 // For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
 
 function whatIsInAName(collection, source) {
-	let sourceKey = Object.keys(source);
-	let newArr = [];
-	collection.filter((obj) => {
-		for (let i = 0; i < sourceKey.length; i++) {
-			if (
-				obj.hasOwnProperty(sourceKey[i]) &&
-				obj[sourceKey[i]] === source[sourceKey[i]]
-			) {
-				newArr.push(obj);
-			}
-		}
-	});
-	// console.log(newArr);
-	return newArr;
+	const sourceKeys = Object.keys(source);
+	return collection.filter((obj) =>
+		sourceKeys.every(
+			(key) => obj.hasOwnProperty(key) && obj[key] === source[key]
+		)
+	);
 }
 
 whatIsInAName(
